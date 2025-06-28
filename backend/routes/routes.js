61e525073dae46f6d474f6f311603de3a83be45d
ashiqe <muhammadashiqe007@gmail.com.com>
2025-06-28 10:35:57 +0530

    updated backend qr controller

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,14 +1,9 @@
 import express from "express";
-import {
-  createShortUrl,
-  generateQRCode,
-  healthCheck,
-  redirectToOriginal,
-} from "../controllers/linkController.js";
+import { createShortUrl, healthCheck, redirectToOriginal } from "../controllers/linkController.js";
 
 export const router = express();
 
 router.post("/url", createShortUrl);
 router.get("/:shortId", redirectToOriginal);
-router.get("/qrcode/:shortId", generateQRCode); 
+// router.get("/qrcode/:shortId", generateQRCode); 
 router.get("/health",healthCheck)
